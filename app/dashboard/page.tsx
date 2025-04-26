c11fd55a58e4b5e0ece508ae645fb154415d33ab
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-04-26 19:56:25 +0530

    Replace auth function with requireUser in Dashboard for improved user authentication

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,10 @@
 import { auth } from "../lib/auth";
 import { redirect } from "next/navigation";
+import { requireUser } from "../lib/hooks";
 
 export default async function Dashboard() {
- const session= await auth();
- if(!session?.user){
-    return  redirect("/")
- }
+ const session= await requireUser();
+ 
     return (
         <>
         <h1>devesh</h1>
