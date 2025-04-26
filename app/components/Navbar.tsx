01ff7d08a45b1ec607dac5036501ac2a9488cd0d
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-04-26 20:10:46 +0530

    Fix Navbar logo image source to correctly display the logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link"
 import Image from "next/image"
+import Logo from '@/public/logo.png'
 export function Navbar(){
     return (
         <div className="flex py-5 items-center justify-between">
            <Link href="/">
-           <Image src=></Image>
+           <Image src= {Logo} alt="logo" className="size-10"></Image>
            </Link>
         </div>
     )
