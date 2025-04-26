ecd04ecb0d75122ecf630a437e3e8622d9117569
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-04-26 20:08:46 +0530

    Refactor code structure for improved readability and maintainability

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+import Image from "next/image"
+export function Navbar(){
+    return (
+        <div className="flex py-5 items-center justify-between">
+           <Link href="/">
+           <Image src=></Image>
+           </Link>
+        </div>
+    )
+}
