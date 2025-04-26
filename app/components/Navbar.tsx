127fa9cdb9734ad5aaecfd72442ce412d79ada94
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-04-26 20:25:10 +0530

    chore: update dependencies and enhance Tailwind CSS configuration
    - Added new dependencies: class-variance-authority, clsx, lucide-react, tailwind-merge, and tailwindcss-animate.
    - Updated Tailwind CSS configuration to support dark mode and extended color palette with new utility classes.
    - Created components.json for component configuration with Shadcn UI.
    - Added utility function `cn` for merging class names using clsx and tailwind-merge.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,9 +4,16 @@ import Logo from '@/public/logo.png'
 export function Navbar(){
     return (
         <div className="flex py-5 items-center justify-between">
-           <Link href="/">
+           <Link href="/" className="flex items-center gap-2">
+           
            <Image src= {Logo} alt="logo" className="size-10"></Image>
+<h4 className="etxt-3xl font-semibold">
+  Dev<span className="text-blue-500">Cal</span>
+
+</h4>
            </Link>
+
+           <button></button>
         </div>
     )
 }
