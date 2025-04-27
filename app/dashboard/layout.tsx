c2d9926833af0f59d514917d03abfa6245d98075
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-04-27 19:37:12 +0530

    feat: add header section with greeting in DashboardLayout component

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -22,6 +22,11 @@ export default function DashboardLayout({children}:{children: ReactNode}){
                     </nav>
                 </div>
             </div>
+
+            <div className="flex flex-col">
+                <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+                <h1>Hello</h1></header>
+            </div>
             </div>
 
         </div>
