e74942c85ac7a418dc272660badaf9d45a3609fc
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-04-27 17:30:49 +0530

    feat: implement authentication check and redirect in Home component

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from "react"
+import Link from "next/link"
+import Image from "next/image"
+import Logo from '@/public/logo.png'
+export default function DashboardLayout({children}:{children: ReactNode}){
+    return (
+        <>
+        <div className="min-h-screen w-full grid md:grid-cols-[220px_1fr] grid-cols-[1fr] lg:grid-cols-[280px_1fr]">
+            <div className="hidden md:block border-r bg-muted/40">
+            <div className="flex h-full max-h-screen flex-col gap-2">
+                <div className="flex h-14 items-center border-b px-4 lg:h-[64px] lg:px-6">
+                    <Link href="/" className="flex items-center gap-2">
+                    <Image src={Logo}  alt="Logo" className="size-6"/>
+                    <p className="text-xl font-bold">Dev<span className="text-primary">Cal</span></p>
+                    </Link>
+                </div>
+
+                <div className="flex-1 ">
+                    <nav className="grid items-start px-2 lg:px-4"></nav>
+                </div>
+            </div>
+            </div>
+
+        </div>
+        </>
+    )
+}
