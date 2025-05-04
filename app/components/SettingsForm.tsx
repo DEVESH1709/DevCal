228a4c8b5e396230c86733388360759be0e58abd
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-05-04 13:14:45 +0530

    feat: implement SettingsForm component and integrate user data retrieval in Settingsroute

---
+++ app/components/SettingsForm.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { CardContent, CardDescription, CardHeader, CardTitle,CardFooter } from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
+import { SubmitButton } from "./SubmitButtons";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
+interface iAppProps{
+    fullName:string;
+    email:string,
+    profileImage:string;
+}
+
+export function SettingsForm({email,fullName,profileImage}:iAppProps){
+    <Card>
+        <CardHeader>
+            <CardTitle></CardTitle>
+            <CardDescription></CardDescription>
+        </CardHeader>
+
+        <form>
+            <CardContent className="flex flex-col gap-y-4">
+                <div className="flex flex-col gap-y-2">
+                    <Label>Full Name</Label>
+                    <Input defaultValue={fullName} placeholder="Devesh Kesharwani"></Input>
+                </div>
+                <div className="flex flex-col gap-y-2">
+                    <Label>Email</Label>
+                    <Input disabled defaultValue={email} placeholder="dev@kirt.com"></Input>
+                </div>
+            </CardContent>
+            <CardFooter><SubmitButton text="Save Changes"></SubmitButton></CardFooter>
+        </form>
+    </Card>
+}
--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -1,4 +1,28 @@
-export default function Settingsroute() {
+import { SettingsForm } from "@/app/components/SettingsForm";
+import prisma from "@/app/lib/db";
 
-    
+async function getData(id:string){
+    const data =await prisma.user.findUnique({
+        where:{
+            id:id,
+        },
+        select:{
+            name:true,
+            email:true,
+            image:true,
+        },
+
+    });
+
+    if(!data){
+        return notFound();
+    }
+    return data;
 }
+
+export default async function Settingsroute() {
+    const session =await requireUser();
+const data =await getData(session.user?.id as string);
+    if(!data){
+    return <SettingsForm email={data.email } fullName={data.name as string} profileImage={data.image as string}></SettingsForm>
+}}
