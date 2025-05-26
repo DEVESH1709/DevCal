4c5085edcbf4674e3924bce03724f0ce1cfbdf8b
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-05-26 12:07:33 +0530

    fix: correct typo in class name for muted foreground in DashboardLinks component

--- app/components/DashboardLinks.tsx
+++ app/components/DashboardLinks.tsx
@@ -47,7 +47,7 @@ return (
     <>
     {dashboardLinks.map((link)=>{
         <Link className={cn(
-            pathname ===link.href? 'text-primary bg-primary/10' :'text-muteed-foreground hover:text-foreground',"flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary"
+            pathname ===link.href? 'text-primary bg-primary/10' :'text-muted-foreground hover:text-foreground',"flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary"
         )} key={link.id} href={link.href}>
             <link.icon className='size-4'></link.icon>
             {link.name}
