362f67559a6ea855bb43549abee62c6d38969570
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-05-27 23:48:13 +0530

    feat: integrate form handling in NewEventRoute with validation and dynamic fields

--- app/dashboard/new/page.tsx
+++ app/dashboard/new/page.tsx
@@ -13,8 +13,13 @@ import { ButtonGroup } from "@/components/ui/ButtonGroup";
 import { useState } from "react";
 import { SubmitButton } from "@/app/components/SubmitButtons";
 import Link from "next/link";
+import { CreateEventTypeAction } from "@/app/actions";
+import { eventTypeSchema } from "@/app/lib/zodSchemas";
+import { useForm } from "@conform-to/react";
+
 
 import { useFormState } from "react-dom";
+import { parseWithZod } from "@conform-to/zod";
 
 type VideoCallProvider = "Zoom Meeting" | "Google Meet" | "Microsoft Teams";
 
@@ -22,6 +27,17 @@ type VideoCallProvider = "Zoom Meeting" | "Google Meet" | "Microsoft Teams";
 export default function NewEventRoute(){
     const [activePlatform,setActivePlatform] =useState<VideoCallProvider>("Google Meet");
     const [lastResult,action]=useFormState(CreateEventTypeAction,undefined)
+    const [form,fields]= useForm({
+        lastResult,
+        onValidate({formData}){
+            return parseWithZod(formData,{
+                schema:eventTypeSchema,
+            });
+        },
+
+        shouldValidate:"onBlur",
+        shouldRevalidate:"onInput",
+    });
     return (
         <div className="w-full h-full flex flex-1 itens-center justify-center">
             <Card>
@@ -32,26 +48,30 @@ export default function NewEventRoute(){
                     </CardDescription>
                 </CardHeader>
 
-                <form>
+                <form id={form.id} onSubmit ={form.onSubmit} action ={action} noValidate>
                     <CardContent className="grid gap-y-5"><div><Label>Title</Label>
-                    <Input placeholder="30 Minute meeting"></Input>
+                    <Input name={fields.title.name} key={fields.title.key} defaultValue={fields.title.initialValue} placeholder="30 Minute meeting"></Input>
+                    <p className="text-red-500 text-sm">{fields.title.errors}</p>
                     </div>
                     <div className="flex flex-col gap-y-2"><Label>URL SLUG</Label>
                     <div className="flex rounded-md">
                         <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-muted bg-muted text-sm text-muted-foreground">DevCal.com </span>
-                        <Input className="rounded-l-none" placeholder="Example-url-1"></Input>
+                        <Input name={fields.url.name} key={fields.url.key} defaultValue={fields.url.initialValue} className="rounded-l-none" placeholder="Example-url-1"></Input>
                     </div>
+                    <p className="text-red-500 text-sm">{fields.url.errors}</p>
                     </div>
 
                     <div className="flex flex-col gap-y-2">
 
                         <Label>Description</Label>
-                        <Textarea placeholder="Meet me in this meeting to meet me! "></Textarea>
+                        <Textarea name={fields.description.name} key={fields.description.key} defaultValue={fields.description.initialValue}  placeholder="Meet me in this meeting to meet me! "></Textarea>
                     </div>
 
                     <div className="flex flex-col gap-y-2">
                         <Label>Duration</Label>
-                        <Select>
+                        <Select
+                        name={fields.duration.name} key={fields.duration.key} defaultValue={fields.duration.initialValue}
+                        >
                             <SelectTrigger>
                                 <SelectValue placeholder="Select duration"></SelectValue>
                             </SelectTrigger>
@@ -65,15 +85,19 @@ export default function NewEventRoute(){
                                </SelectGroup>
                             </SelectContent>
                         </Select>
+
+                        <p className="text-red-500 text-sm">{fields.duration.errors}</p>
                     </div>
                     <div className="grid gap-y-2">
                         <Label>Video Call Provider</Label>
+                        <input type="hidden" name={fields.videoCallSoftware.name} value={activePlatform}></input>
                         <ButtonGroup  >
                             <Button type="button" onClick={ ()=>setActivePlatform("Zoom Meeting")} className="w-full" variant={activePlatform==="Zoom Meeting"? "secondary":"outline"}>Zoom</Button>
                              <Button  type="button"  onClick={ ()=>setActivePlatform("Google Meet")} className="w-full" variant={activePlatform==="Google Meet"? "secondary":"outline"}>Google Meet</Button>
 
                               <Button type="button"  onClick={ ()=>setActivePlatform("Microsoft Teams")}className="w-full" variant={activePlatform==="Microsoft Teams"? "secondary":"outline"}>Microsoft Teams</Button>
                         </ButtonGroup>
+                         <p className="text-red-500 text-sm">{fields.VideoCallProvider.errors}</p>
                     </div>
                     </CardContent>
 
