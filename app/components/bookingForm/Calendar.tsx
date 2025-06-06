de15042a86df9b297f837693ebb7eae27aa8bb30
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-06-07 00:34:52 +0530

    feat: integrate CalendarHeader into Calendar component for improved display

--- app/components/bookingForm/Calendar.tsx
+++ app/components/bookingForm/Calendar.tsx
@@ -2,6 +2,7 @@ import {useCalendar, useLocale} from "react-aria";
 import {useCalendarState} from 'react-stately';
 import {createCalendar} from '@internationalized/date';
 import {CalendarProps,DateValue} from "@react-types/calendar"
+import { CalendarHeader } from "./CalendarHeader";
 export function Calendar(props:CalendarProps<DateValue>){
     const {locale} =useLocale();
   let state = useCalendarState({
@@ -14,6 +15,8 @@ export function Calendar(props:CalendarProps<DateValue>){
   let {calendarProps, prevButtonProps, nextButtonProps, title} = useCalendar(props, state);
     return (
         
-        <div {...calendarProps} className="inline-block"></div>
+        <div {...calendarProps} className="inline-block">
+            <CalendarHeader state={state} calendarProps={calendarProps} prevButtonProps={prevButtonProps} nextButtonProps={nextButtonProps}></CalendarHeader>
+        </div>
     )
 }
