1d50346e7040fbc9dbb4cca6571a960e58385c57
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-06-07 00:13:39 +0530

    feat: implement Calendar component with state management and props integration

--- app/components/bookingForm/Calendar.tsx
+++ app/components/bookingForm/Calendar.tsx
@@ -1,6 +1,19 @@
-export function Calendar(){
+import {useCalendar, useLocale} from "react-aria";
+import {useCalendarState} from 'react-stately';
+import {createCalendar} from '@internationalized/date';
+import {CalendarProps,DateValue} from "@react-types/calendar"
+export function Calendar(props:CalendarProps<DateValue>){
+    const {locale} =useLocale();
+  let state = useCalendarState({
+    ...props,
+    visibleDuration: {months: 1},
+    locale,
+    createCalendar,
+  });
+
+  let {calendarProps, prevButtonProps, nextButtonProps, title} = useCalendar(props, state);
     return (
         
-        <div className=" "></div>
+        <div {...calendarProps} className="inline-block"></div>
     )
 }
