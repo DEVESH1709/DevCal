e398d69a79a0a169aab1c925b35e42584bbdea59
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-06-18 19:20:50 +0530

    feat: implement isDateUnavailable function in RenderCalendar for improved date availability handling

--- app/components/bookingForm/RenderCalendar.tsx
+++ app/components/bookingForm/RenderCalendar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Calendar } from "./Calendar"
-
+import {CalendarProps,DateValue} from '@react-types/calendar'
 import {today,getLocalTimeZone} from "@internationalized/date"
 
 interface iAppProps {
@@ -10,7 +10,13 @@ interface iAppProps {
         isActive:boolean;
     }[];
 }
-export function RenderCalendar({availability}:iAppProps){4
+export function RenderCalendar({availability}:iAppProps){
     
-    return <Calendar minValue={today(getLocalTimeZone())}></Calendar>
-}
+    const isDateUnavailable =(date:DateValue)=>{
+         const dayOfWeek  = date.toDate(getLocalTimeZone()).getDay();
+         const adjustedIndex = dayOfWeek ===0 ?6 : dayOfWeek-1;
+         return !availability[adjustedIndex].isActive;
+    }
+
+return <Calendar minValue={today(getLocalTimeZone())} isDateUnavailable={isDateUnavailable}></Calendar>
+}                                                                                   
