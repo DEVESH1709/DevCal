e548703a49b20ee06ae0b0f38bd303c9e32dc399
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-06-25 03:26:16 +0530

    feat: add date-fns dependency and implement date formatting in TimeTable component

--- app/components/bookingForm/TimeTable.tsx
+++ app/components/bookingForm/TimeTable.tsx
@@ -1,3 +1,6 @@
+import {format} from "date-fns";
+
+
 interface iAppProps{
     selectedDate :Date;
 }
@@ -6,7 +9,13 @@ interface iAppProps{
 export function TimeTable({selectedDate}:iAppProps){
     return(
         <div>
-            <p></p>
+            <p className="text-base font-semibold">{format(selectedDate,"EEE")}{" "}
+                <span className="text-sm text-muted-foreground">
+                    {format(selectedDate,"MMM. d")}
+                </span>
+
+
+            </p>
         </div>
     )
 }
