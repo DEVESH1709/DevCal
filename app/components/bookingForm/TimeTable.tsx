f8b45c64824afd890b86bcd3677de035c88e49ef
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-06-25 03:34:26 +0530

    feat: refactor TimeTable component to include userName and fetch availability data

--- app/components/bookingForm/TimeTable.tsx
+++ app/components/bookingForm/TimeTable.tsx
@@ -1,12 +1,35 @@
+import prisma from "@/app/lib/db";
 import {format} from "date-fns";
 
+async function getData(userName:string){
+    const data =await prisma.availability.findFirst({
+        where:{
+            day:"Monday",
+            User:{
+                userName:userName,
+            },
+        },
+        select :{
+            firstTime:true,
+            tillTime:true,
+            id:true,
+            User:{
+                grantEmail:true,
+                grant:true,
+            }
+        }
+    });
+    return data;
+}
 
 interface iAppProps{
     selectedDate :Date;
+    userName:string;
 }
 
 
-export function TimeTable({selectedDate}:iAppProps){
+export async function TimeTable({selectedDate,userName}:iAppProps){
+    const data =await getData(userName);
     return(
         <div>
             <p className="text-base font-semibold">{format(selectedDate,"EEE")}{" "}
