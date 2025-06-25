02e1c9f85234a8ae602e3ba2aa089f795285ed52
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-06-26 02:19:36 +0530

    feat: enhance getData function to include selectedDate and fetch availability data from Nylas

--- app/components/bookingForm/TimeTable.tsx
+++ app/components/bookingForm/TimeTable.tsx
@@ -1,10 +1,20 @@
 import prisma from "@/app/lib/db";
-import {format} from "date-fns";
+import {format, parse} from "date-fns";
+import {Prisma} from "@prisma/client";
+import {nylas} from "@/app/lib/nylas";
 
-async function getData(userName:string){
+
+async function getData(userName:string,selectedDate:Date){
+    const currentDay =format(selectedDate,"EEEE");
+
+    const startOfDay =new Date(selectedDate);
+    startOfDay.setHours(0,0,0,0);  // hrs, minutes, seconds, milliseconds
+
+    const endOfDay =new Date(selectedDate);
+    endOfDay.setHours(23,59,59,999);  // hrs, minutes, seconds, milliseconds
     const data =await prisma.availability.findFirst({
         where:{
-            day:"Monday",
+            day:currentDay as Prisma.EnumDayFilter,
             User:{
                 userName:userName,
             },
@@ -21,7 +31,20 @@ async function getData(userName:string){
             },
         },
     });
-    return data;
+
+
+    const nylasCalendarData = await nylas.calendars.getFreeBusy({
+        identifier: data?.User?.grantId as string,
+        requestBody:{
+   startTime :Math.floor(startOfDay.getTime()/1000),
+    endTime :Math.floor(endOfDay.getTime()/1000),
+    emails: [data?.User?.grantEmail as string],
+        }
+    })
+    return {
+        data,
+        nylasCalendarData,
+    };
 }
 
 interface iAppProps{
@@ -29,9 +52,29 @@ interface iAppProps{
     userName:string;
 }
 
+function calculateAvailableTimeSlots(){
+    date:string,
+    dbAvailability:{
+        fromTime :string | undefined;
+        tillTime :string | undefined;
+    },
+    nylasData:NylasResponse
+    const now =new Date();
+    const availableFrom = parse(
+        `${date} ${dbAvailability.fromTime}`,"yyyy-MM-dd HH:mm",
+        new Date()
+    );
+
+    const availableTill = parse(`${date} ${dbAvailability.tillTime}`,
+        "yyyy-MM-dd HH:mm",
+        new Date()
+    );
+    const busySlots = 
+
+}
 
 export async function TimeTable({selectedDate,userName}:iAppProps){
-    const data =await getData(userName);
+    const {data,nylasCalendarData} =await getData(userName,selectedDate);
     return(
         <div>
             <p className="text-base font-semibold">{format(selectedDate,"EEE")}{" "}
